668becb81d657ef9818c91dc4d480de12ad84343
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-07-20 13:24:55 +0300

    refactor: update props

--- Online-Store/src/modules/main/filters/FilterSlider/FilterSlider.tsx
+++ Online-Store/src/modules/main/filters/FilterSlider/FilterSlider.tsx
@@ -17,32 +17,32 @@ type FilterSliderProps = {
 	maxValue: number,
 	defaultValue: [number, number],
 }
-export function FilterSlider(props: FilterSliderProps) {
+export function FilterSlider({name, title, maxValue, minValue, defaultValue}: FilterSliderProps) {
 	const { filter, changeFilterSlider } = dataSlice.actions;
 	const { filters } = useAppSelector(state => state.dataReducer);
 	const dispatch = useAppDispatch();
 	const { color } = useAppSelector(state => state.dataReducer);
 	return <div className={style.wrapper}>
-		<h4 className={style.title}>{props.title}:</h4>
+		<h4 className={style.title}>{title}:</h4>
 		{/* {props.defaultValue[0] + '  ' + props.defaultValue[1]} */}
 		<div className={style.value_wrapper}>
-			<div><p className={style.value_p}>{props.defaultValue[0]}</p></div>
-			<div><p className={style.value_p}>{props.defaultValue[1]}</p></div>
+			<div><p className={style.value_p}>{defaultValue[0]}</p></div>
+			<div><p className={style.value_p}>{defaultValue[1]}</p></div>
 		</div>
 		<RangeSlider
 			aria-label={['min', 'max']}
-			min={props.minValue}
-			max={props.maxValue}
-			defaultValue={props.defaultValue}
+			min={minValue}
+			max={maxValue}
+			defaultValue={defaultValue}
 			onChange={((val: [number, number]) => {
-				dispatch(changeFilterSlider({ key: props.name, value: val }));
+				dispatch(changeFilterSlider({ key: name, value: val }));
 			})}
 			onChangeEnd={((val: [number, number]) => {
-				dispatch(changeFilterSlider({ key: props.name, value: val }));
+				dispatch(changeFilterSlider({ key: name, value: val }));
 				dispatch(filter());
 			})}
 
-			value={props.defaultValue}
+			value={defaultValue}
 
 			colorScheme="cyan"
 			width="100%"
@@ -83,10 +83,10 @@ export function FilterSlider(props: FilterSliderProps) {
 				cursor='pointer'
 				onKeyDown={(ev) => {
 					if (ev.code === 'ArrowLeft') {
-						dispatch(changeFilterSlider({ key: props.name, value: [filters[props.name][0], filters[props.name][1] - 1] }));
+						dispatch(changeFilterSlider({ key: name, value: [filters[name][0], filters[name][1] - 1] }));
 					}
 					if (ev.code === 'ArrowRight') {
-						dispatch(changeFilterSlider({ key: props.name, value: [filters[props.name][0], filters[props.name][1] + 1] }));
+						dispatch(changeFilterSlider({ key: name, value: [filters[name][0], filters[name][1] + 1] }));
 					}
 				}}
 			/>
